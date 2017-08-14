b489d4c128c2c24180713e5eb5ca0cc47f693fec
Conor Doyle <doyle7@hotmail.com>
2017-08-14 13:57:41 +0100

    Add 'Duck' container and actions.

---
+++ app/helpers/api.js
@@ -0,0 +1,60 @@
+import { ref } from 'config/constants'
+
+function saveToDucks (duck) {
+  const duckId = ref.child('ducks').push().key
+  const duckPromise = ref.child(`ducks/${duckId}`).set({...duck, duckId})
+  return {
+    duckId,
+    duckPromise,
+  }
+}
+
+function saveToUsersDucks (duck, duckId) {
+  return ref.child(`usersDucks/${duck.uid}/${duckId}`)
+    .set({...duck, duckId})
+}
+
+function saveLikeCount (duckId) {
+  return ref.child(`likeCount/${duckId}`).set(0)
+}
+
+export function saveDuck (duck) {
+  const { duckId, duckPromise } = saveToDucks(duck)
+
+  return Promise.all([
+    duckPromise,
+    saveToUsersDucks(duck, duckId),
+    saveLikeCount(duckId),
+  ]).then(() => ({...duck, duckId}))
+}
+
+export function listenToFeed (cb, errorCB) {
+  ref.child('ducks').on('value', (snapshot) => {
+    const feed = snapshot.val() || {}
+    const sortedIds = Object.keys(feed).sort((a, b) => feed[b].timestamp - feed[a].timestamp)
+    cb({feed, sortedIds})
+  }, errorCB)
+}
+
+export function fetchUsersLikes (uid) {
+  return ref.child(`usersLikes/${uid}`).once('value')
+    .then((snapshot) => snapshot.val() || {})
+}
+
+export function saveToUsersLikes (uid, duckId) {
+  return ref.child(`usersLikes/${uid}/${duckId}`).set(true)
+}
+
+export function deleteFromUsersLikes (uid, duckId) {
+  return ref.child(`usersLikes/${uid}/${duckId}`).set(null)
+}
+
+export function incrementNumberOfLikes (duckId) {
+  return ref.child(`likeCount/${duckId}`)
+    .transaction((currentValue = 0) => currentValue + 1)
+}
+
+export function decrementNumberOfLikes (duckId) {
+  return ref.child(`likeCount/${duckId}`)
+    .transaction((currentValue = 0) => currentValue - 1)
+}
