ddeb0a440f9cf799e011ac66921d0c4f6597d502
Conor Doyle <doyle7@hotmail.com>
2017-08-14 13:47:53 +0100

    Add Thunk middleware.

--- app/containers/Authenticate/AuthenticateContainer.js
+++ app/containers/Authenticate/AuthenticateContainer.js
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import { Authenticate } from 'components'
 import auth from 'helpers/auth'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import * as userActionCreators from 'redux/modules/users'
 
 const AuthenticateContainer = React.createClass({
+  propTypes: {
+    fetchAndHandleAuthedUser: PropTypes.func.isRequired,
+    isFetching: PropTypes.bool.isRequired,
+    error: PropTypes.string.isRequired,
+  },
   handleAuth () {
-    auth().then((user) => {
-      console.log(user)
-    })
+    this.props.fetchAndHandleAuthedUser()
   },
   render () {
     return (
       <Authenticate
         onAuth={this.handleAuth}
-        isFetching={false}
-        error={''} />
+        isFetching={this.props.isFetching}
+        error={this.props.error} />
     )
   },
 })
 
-export default AuthenticateContainer
+export default connect(
+  (state) => ({isFetching: state.isFetching, error: state.error}),
+  (dispatch) => bindActionCreators(userActionCreators, dispatch)
+)(AuthenticateContainer)
--- app/index.js
+++ app/index.js
@@ -1,4 +1,15 @@
+import React from 'react'
 import ReactDOM from 'react-dom'
 import routes from 'config/routes'
+import users from 'redux/modules/users'
+import thunk from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(routes, document.getElementById('app'))
+const store = createStore(users, applyMiddleware(thunk))
+
+ReactDOM.render(
+  <Provider store={store}>
+    {routes}
+  </Provider>,
+document.getElementById('app'))
