85388591ab70bba5144b4363426bf1d6f572e2f2
Conor Doyle <doyle7@hotmail.com>
2017-08-26 21:03:35 +0100

    Update dependencies.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,62 +1,65 @@
-import webpack from 'webpack'
-import path from 'path'
-import HtmlWebpackPlugin from 'html-webpack-plugin'
+import webpack from 'webpack';
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-const LAUNCH_COMMAND = process.env.npm_lifecycle_event
+const LAUNCH_COMMAND = process.env.npm_lifecycle_event;
 
-const isProduction = LAUNCH_COMMAND === 'production'
-process.env.BABEL_ENV = LAUNCH_COMMAND
+const isProduction = LAUNCH_COMMAND === 'production';
+process.env.BABEL_ENV = LAUNCH_COMMAND;
 
 const PATHS = {
-  app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'dist'),
-}
+  src: path.join(__dirname, 'src'),
+  build: path.join(__dirname, 'build')
+};
 
 const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: PATHS.app + '/index.html',
+  template: `${PATHS.src}/index.html`,
   filename: 'index.html',
-  inject: 'body',
-})
+  inject: 'body'
+});
 
 const productionPlugin = new webpack.DefinePlugin({
   'process.env': {
-    NODE_ENV: JSON.stringify('production'),
-  },
-})
+    NODE_ENV: JSON.stringify('production')
+  }
+});
 
 const base = {
   entry: [
-    PATHS.app,
+    PATHS.src
   ],
   output: {
     path: PATHS.build,
-    filename: 'index_bundle.js',
+    filename: 'index_bundle.js'
   },
   module: {
     loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
-      {test: /\.css$/, loader: 'style!css?sourceMap&modules&localIdentName=[name]__[local]___[hash:base64:5]'},
-    ],
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, loader: 'style-loader!css-loader?sourceMap&modules&localIdentName=[name]__[local]___[hash:base64:5]' }
+    ]
   },
   resolve: {
-    root: path.resolve('./app'),
-  },
-}
+    modules: [
+      path.resolve(__dirname, 'src'), 'node_modules'
+    ]
+  }
+};
 
 const developmentConfig = {
   devtool: 'cheap-module-inline-source-map',
   devServer: {
     contentBase: PATHS.build,
     hot: true,
-    inline: true,
-    progress: true,
+    inline: true
   },
-  plugins: [HTMLWebpackPluginConfig, new webpack.HotModuleReplacementPlugin()],
-}
+  plugins: [HTMLWebpackPluginConfig, new webpack.HotModuleReplacementPlugin()]
+};
 
 const productionConfig = {
   devtool: 'cheap-module-source-map',
-  plugins: [HTMLWebpackPluginConfig, productionPlugin],
-}
+  plugins: [HTMLWebpackPluginConfig, productionPlugin]
+};
 
-export default Object.assign({}, base, isProduction === true ? productionConfig : developmentConfig)
+export default Object.assign({}, base, isProduction === true
+  ? productionConfig
+  : developmentConfig);
