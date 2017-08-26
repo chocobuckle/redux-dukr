3f01dec0b42c0458560635340aa482e8ec92adfe
Conor Doyle <doyle7@hotmail.com>
2017-08-26 21:04:31 +0100

    Add '.eslintignore' file and ignore 'build' folder for the moment

--- app/components/Logout/Logout.js
+++
@@ -1,8 +0,0 @@
-import React, { PropTypes } from 'react'
-import { text } from './styles.css'
-
-export default function Logout () {
-  return (
-    <div className={text}>{'You are now logged out'}</div>
-  )
-}
