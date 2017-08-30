e7818564d78b619e33ab665d2489de011febba80
Conor Doyle <doyle7@hotmail.com>
2017-08-30 10:54:13 +0100

    Update 'Home' initial text.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={container}>
       <p className={title}>{'Duckr'}</p>
-      <p className={slogan}>{'The real time, cloud based, modular, scalable, growth hack, social platform. In the cloud.'}</p>
+      <p className={slogan}>{'A real time, cloud-based, growth hacking, disrupting, modular and scalable social platform! All the buzzwords venture capitalists like! Sigh.'}</p>
     </div>
   );
 }
