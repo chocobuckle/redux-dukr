b356b290863367d21eee47a61aae9e43db3dd1d6
Conor Doyle <conorfrontend@gmail.com>
2018-01-18 01:34:00 +0000

    Add 'ReplyForm' component and container

---
+++ src/components/ReplyForm.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { func, string } from 'prop-types';
+import styled from 'styled-components';
+import { darkBtn, baseTextAreaContainer, baseTextArea } from 'sharedStyles/styles';
+
+ReplyForm.propTypes = {
+  handleOnChange: func.isRequired,
+  handleSubmit: func.isRequired,
+  text: string.isRequired
+};
+
+function ReplyForm({ handleOnChange, handleSubmit, text }) {
+  return (
+    <ReplyTextAreaContainer>
+      <ReplyTextArea
+        onChange={e => handleOnChange(e)}
+        maxLength={140}
+        type='text'
+        value={text}
+        placeholder='Your reponse'
+      />
+      <Button onClick={(e) => handleSubmit(e)}>Submit</Button>
+    </ReplyTextAreaContainer>
+  );
+}
+
+const ReplyTextAreaContainer = styled.div`
+  ${baseTextAreaContainer};
+  display: flex;
+  flex-direction: column;
+  height: 160px;
+  margin: 15px auto;
+  width: 70%;
+`;
+
+const ReplyTextArea = styled.textarea`
+  ${baseTextArea}
+  border: 1px solid #ccc;
+  margin: 10px 0;
+`;
+
+const Button = styled.button`
+  ${darkBtn}
+`;
+
+export default ReplyForm;
---
+++ src/containers/ReplyFormContainer.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { func } from 'prop-types';
+import { ReplyForm } from 'components';
+
+class ReplyFormContainer extends Component {
+  static propTypes = {
+    submit: func.isRequired
+  };
+
+  state = {
+    text: ''
+  };
+
+  handleOnChange = (e) => {
+    const text = e.target.value;
+    this.setState({
+      text
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { text } = this.state;
+    if (text.length === 0) return;
+    this.props.submit(text);
+    this.setState({
+      text: ''
+    });
+  };
+
+  render() {
+    return (
+      <ReplyForm
+        handleOnChange={this.handleOnChange}
+        handleSubmit={this.handleSubmit}
+        text={this.state.text}
+      />
+    )
+  }
+}
+
+export default ReplyFormContainer;
